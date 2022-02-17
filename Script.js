b9baba86eb4987e4774e145ff5816d2e96dc38e3
Konemui <79459504+Konemui@users.noreply.github.com>
2022-02-17 20:50:57 +0000

    Fix and testing js
    -fixed incorrect file link
    -testing background colour change

--- Script.js
+++ Script.js
@@ -1,1 +1,7 @@
-//placeholder file.
+document.getElementById("chrome").addEventListener("mouseover", function() {
+    document.getElementById("chrome").style.backgroundColor = "black";
+});
+    
+document.getElementById("chrome").addEventListener("mouseout", function() {
+    document.getElementById("chrome").style.backgroundColor = "white";
+});
