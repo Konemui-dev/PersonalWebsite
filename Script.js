b466355f5fe2fac27078a4e0a3ea321c20bfd796
Konemui <79459504+Konemui@users.noreply.github.com>
2022-02-19 20:26:03 +0000

    Main content sections
    Skeleton for main body added + some styling testing

--- Script.js
+++ Script.js
@@ -5,3 +5,4 @@ document.getElementById("chrome").addEventListener("mouseover", function() {
 document.getElementById("chrome").addEventListener("mouseout", function() {
     document.getElementById("chrome").style.backgroundColor = "white";
 });
+
